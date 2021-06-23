0f0d7b248c6ef8c9afc7637a387eb766da118d3d
Sumuyzzz <Sumuyzzz@gmail.com>
2021-06-23 23:37:19 +0800

    deploy

---
+++ precache-manifest.852332f5c5698bbb496c543d79e7ce3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67621528aba338416217",
+    "url": "/money_manage/css/app.a9b3baab.css"
+  },
+  {
+    "revision": "9ac706133f305dc0db2a88b302501846",
+    "url": "/money_manage/index.html"
+  },
+  {
+    "revision": "67621528aba338416217",
+    "url": "/money_manage/js/app.4c13facb.js"
+  },
+  {
+    "revision": "60104d57467c9b4c2efb",
+    "url": "/money_manage/js/chunk-vendors.820ec289.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/money_manage/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money_manage/robots.txt"
+  }
+]);
